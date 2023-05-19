24cf875757d88993cce173bb564251ececeab319
Peterson Maranho Santos <44443488+Voltty@users.noreply.github.com>
2023-05-19 14:14:58 -0300

    API implementado
    -login e cadastro funcionando

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -65,18 +65,21 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
+    var modalidade = req.body.modalidadeServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
-        res.status(400).send("Seu nome está undefined!");
+        res.status(400).send("Você não inseriu seu nome!");
     } else if (email == undefined) {
-        res.status(400).send("Seu email está undefined!");
+        res.status(400).send("Você não inseriu seu email!");
     } else if (senha == undefined) {
-        res.status(400).send("Sua senha está undefined!");
+        res.status(400).send("Você não inseriu sua senha!");
+    }else if (modalidade == undefined) {
+            res.status(400).send("Você não escolheu uma modalidade!");
     } else {
         
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, modalidade)
             .then(
                 function (resultado) {
                     res.json(resultado);
